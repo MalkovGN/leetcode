804362f97c226cd475282b964f5f6531b2fdba0f
MalkovGN <malkovgn@gmail.com>
2024-01-25 12:25:13 +0300

    25.01 daily problem

--- main.py
+++ main.py
@@ -782,5 +782,16 @@ def problem70(n: int) -> int:
     return 1 if n == 1 else curr
 
 
+def problem1143(text1: str, text2: str) -> int:
+    dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+    for i in range(1, len(text1) + 1):
+        for j in range(1, len(text2) + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[len(text1)][len(text2)]
+
+
 if __name__ == '__main__':
-    print(problem70(2))
+    print(problem1143('abcde', 'ace'))
